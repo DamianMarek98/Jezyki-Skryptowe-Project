77587591159971e0071afa7a1b7e94655d23f50d
Damian Marek <zmdddamian@gmail.com>
2021-04-11 20:16:46 +0200

    board logic fixes and extensions

--- Project_3/logic/Board.py
+++ Project_3/logic/Board.py
@@ -1,125 +1,175 @@
+import os
 from enum import Enum
+from pathlib import Path
+from logic.Enums import *
 
 
 class Board():
+
     def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
-        self.__turn = 0
-        self.__fields = Field[[Field.EMPTY in range(x)] in range(y)]  # todo check if it works
-        self.__base_fields = Field[[Field.EMPTY in range(x)] in range(y)]
-        self.__player_pos_x = 0
-        self.__player_pos_y = 0
-        self.__field_under_player = 0
+        self.x: int = x
+        self.y: int = y
+        self.turn = 0
+        self.fields = [[Field.EMPTY for i in range(x)] for j in range(y)]  # todo check if it works
+        self.base_fields = [[Field.EMPTY for i in range(x)] for j in range(y)]
+        self.player_pos_x = 0
+        self.player_pos_y = 0
+        self.field_under_player = Field.EMPTY
+        self.chests = 0
+        self.chests_on_goals = 0
 
     def set_field_type(self, x, y, field):
-        self.__fields[x][y] = field
+        self.fields[x][y] = field
 
     def player_move(self, direction):
         if direction == Direction.UP and self.check_player_up_collision():
-            self.__fields[self.__player_pos_x][self.__player_pos_y] = self.__field_under_player
-            self.__player_pos_x -= 1
+            self.fields[self.player_pos_x][self.player_pos_y] = self.field_under_player
+            self.player_pos_x -= 1
             self.move_player()
 
         elif direction == Direction.DOWN and self.check_player_down_collision():
-            self.__fields[self.__player_pos_x][self.__player_pos_y] = self.__field_under_player
-            self.__player_pos_x += 1
+            self.fields[self.player_pos_x][self.player_pos_y] = self.field_under_player
+            self.player_pos_x += 1
             self.move_player()
 
         elif direction == Direction.LEFT and self.check_player_left_collision():
-            self.__fields[self.__player_pos_x][self.__player_pos_y] = self.__field_under_player
-            self.__player_pos_y -= 1
+            self.fields[self.player_pos_x][self.player_pos_y] = self.field_under_player
+            self.player_pos_y -= 1
             self.move_player()
 
         elif direction == Direction.RIGHT and self.check_player_right_collision():
-            self.__fields[self.__player_pos_x][self.__player_pos_y] = self.__field_under_player
-            self.__player_pos_y += 1
+            self.fields[self.player_pos_x][self.player_pos_y] = self.field_under_player
+            self.player_pos_y += 1
             self.move_player()
 
     def move_player(self):
-        self.__field_under_player = self.__fields[self.__player_pos_x][self.__player_pos_y]
-        self.__fields[self.__player_pos_x][self.__player_pos_y] = Field.PLAYER
+        self.field_under_player = self.fields[self.player_pos_x][self.player_pos_y]
+        self.fields[self.player_pos_x][self.player_pos_y] = Field.PLAYER
+        self.count_chests_on_goals()
 
     def check_player_up_collision(self) -> bool:
         if self.get_field_based_on_player_pos(-1, 0) == Field.WALL:
             return False
-        elif self.get_field_based_on_player_pos(-1, 0) == Field.CHEST and self.check_chest_collision(
-                self.__player_pos_x - 2, self.__player_pos_y):
-            self.__fields[self.__player_pos_x - 2][self.__player_pos_y] = Field.CHEST
-            self.__fields[self.__player_pos_x - 1][self.__player_pos_y] = self.__base_fields[self.__player_pos_x - 1][
-                self.__player_pos_y]
-            return True
+        elif self.get_field_based_on_player_pos(-1, 0) == Field.CHEST:
+            if self.check_chest_collision(self.player_pos_x - 2, self.player_pos_y):
+                self.fields[self.player_pos_x - 2][self.player_pos_y] = Field.CHEST
+                self.fields[self.player_pos_x - 1][self.player_pos_y] = self.base_fields[self.player_pos_x - 1][
+                    self.player_pos_y]
+                return True
+            else:
+                return False
         else:
             return True
 
     def check_player_down_collision(self) -> bool:
         if self.get_field_based_on_player_pos(1, 0) == Field.WALL:
             return False
-        elif self.get_field_based_on_player_pos(1, 0) == Field.CHEST and self.check_chest_collision(
-                self.__player_pos_x + 2, self.__player_pos_y):
-            self.__fields[self.__player_pos_x + 2][self.__player_pos_y] = Field.CHEST
-            self.__fields[self.__player_pos_x + 1][self.__player_pos_y] = self.__base_fields[self.__player_pos_x + 1][
-                self.__player_pos_y]
-            return True
+        elif self.get_field_based_on_player_pos(1, 0) == Field.CHEST:
+            if self.check_chest_collision(self.player_pos_x + 2, self.player_pos_y):
+                self.fields[self.player_pos_x + 2][self.player_pos_y] = Field.CHEST
+                self.fields[self.player_pos_x + 1][self.player_pos_y] = self.base_fields[self.player_pos_x + 1][
+                    self.player_pos_y]
+                return True
+            else:
+                return False
         else:
             return True
 
     def check_player_left_collision(self) -> bool:
         if self.get_field_based_on_player_pos(0, -1) == Field.WALL:
             return False
-        elif self.get_field_based_on_player_pos(0, -1) == Field.CHEST and self.check_chest_collision(
-                self.__player_pos_x, self.__player_pos_y - 2):
-            self.__fields[self.__player_pos_x][self.__player_pos_y - 2] = Field.CHEST
-            self.__fields[self.__player_pos_x][self.__player_pos_y - 1] = self.__base_fields[self.__player_pos_x][
-                self.__player_pos_y - 1]
-            return True
+        elif self.get_field_based_on_player_pos(0, -1) == Field.CHEST:
+            if self.check_chest_collision(self.player_pos_x, self.player_pos_y - 2):
+                self.fields[self.player_pos_x][self.player_pos_y - 2] = Field.CHEST
+                self.fields[self.player_pos_x][self.player_pos_y - 1] = self.base_fields[self.player_pos_x][
+                    self.player_pos_y - 1]
+                return True
+            else:
+                return False
         else:
             return True
 
     def check_player_right_collision(self) -> bool:
         if self.get_field_based_on_player_pos(0, 1) == Field.WALL:
             return False
-        elif self.get_field_based_on_player_pos(0, 1) == Field.CHEST and self.check_chest_collision(
-                self.__player_pos_x, self.__player_pos_y + 2):
-            self.__fields[self.__player_pos_x][self.__player_pos_y + 2] = Field.CHEST
-            self.__fields[self.__player_pos_x][self.__player_pos_y + 1] = self.__base_fields[self.__player_pos_x][
-                self.__player_pos_y + 1]
-            return True
+        elif self.get_field_based_on_player_pos(0, 1) == Field.CHEST:
+            if self.check_chest_collision(self.player_pos_x, self.player_pos_y + 2):
+                self.fields[self.player_pos_x][self.player_pos_y + 2] = Field.CHEST
+                self.fields[self.player_pos_x][self.player_pos_y + 1] = self.base_fields[self.player_pos_x][
+                    self.player_pos_y + 1]
+                return True
+            else:
+                return False
         else:
             return True
 
     def check_chest_collision(self, x, y) -> bool:
-        if self.__fields[x][y] == Field.WALL or self.__fields[x][y] == Field.CHEST:
+        if self.fields[x][y] == Field.WALL or self.fields[x][y] == Field.CHEST:
             return False
 
         return True
 
     def get_field_based_on_player_pos(self, x, y):
-        return self.__fields[self.__player_pos_x + x][self.__player_pos_y + y]
+        return self.fields[self.player_pos_x + x][self.player_pos_y + y]
 
     def get_x(self):
-        return self.__x
+        return self.x
 
     def get_y(self):
-        return self.__y
+        return self.y
 
     def get_turn(self):
-        return self.__turn
+        return self.turn
 
     def get_board(self):
-        return self.__fields
+        return self.fields
+
+    def initialize_board_fields(self, x, y):
+        self.x = x
+        self.y = y
+        self.turn = 0
+        self.fields = [[Field.EMPTY for i in range(x)] for j in range(y)]  # todo check if it works
+        self.base_fields = [[Field.EMPTY for i in range(x)] for j in range(y)]
+        self.player_pos_x = 0
+        self.player_pos_y = 0
+        self.field_under_player = Field.EMPTY
+
+    def load_board_from_file(self, file_name):
+        with open(
+                os.path.dirname(Path(__file__).parent) + '\\boards\\' + file_name) as file:
+            x = int(file.readline().strip())
+            y = int(file.readline().strip())
+            self.chests = 0
+            self.chests_on_goals = 0
+            self.initialize_board_fields(x, y)
+            for i in range(0, self.x):
+                for j in range(0, self.y):
+                    read_field = int(file.read(1))
+                    self.fields[i][j] = Field(int(read_field))
+                    self.base_fields[i][j] = Field(int(read_field))
+                    if read_field == 3:
+                        self.base_fields[i][j] = Field.WALL
+                        self.player_pos_x = i
+                        self.player_pos_y = j
+
+                    if read_field == 2:
+                        self.base_fields[i][j] = Field.EMPTY
+                        self.chests += 1
+
+            file.close()
+
+    def check_if_player_won(self) -> bool:
+        return self.chests == self.chests_on_goals
+
+    def count_chests_on_goals(self):
+        self.chests_on_goals = 0
+        for i in range(0, self.x):
+            for j in range(0, self.y):
+                if self.fields[i][j] == Field.CHEST and self.base_fields[i][j] == Field.GOAL:
+                    self.chests_on_goals += 1
+
+    def return_result(self) -> str:
+        return 'Wynik: ' + str(self.chests_on_goals) + ' z ' + str(self.chests)
 
 
-class Field(Enum):
-    EMPTY = 0,
-    WALL = 1,
-    CHEST = 2,
-    PLAYER = 3,
-    GOAL = 4
 
-
-class Direction(Enum):
-    UP = 0,
-    DOWN = 1,
-    LEFT = 2,
-    RIGHT = 3
