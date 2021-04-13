9e0b897eb3430783fc940bae66fb4ee86aee777d
Damian Marek <zmdddamian@gmail.com>
2021-04-13 02:04:07 +0200

    gtk interface init

--- Project_3/interface/main.py
+++ Project_3/interface/main.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+import cairo
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import GLib, Gio, Gtk, GdkPixbuf
+
+TEST_XML = """
 <?xml version="1.0"?>
 <interface>
   <requires lib="gtk+" version="2.16"/>
@@ -233,3 +242,87 @@
     <property name="stock">gtk-missing-image</property>
   </object>
 </interface>
+
+"""
+
+class AppWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class Application(Gtk.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            application_id="org.example.myapp",
+            flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
+            **kwargs
+        )
+        self.window = None
+
+        self.add_main_option(
+            "test",
+            ord("t"),
+            GLib.OptionFlags.NONE,
+            GLib.OptionArg.NONE,
+            "Command line test",
+            None,
+        )
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+        action = Gio.SimpleAction.new("about", None)
+        action.connect("activate", self.on_about)
+        self.add_action(action)
+
+        action = Gio.SimpleAction.new("quit", None)
+        action.connect("activate", self.on_quit)
+        self.add_action(action)
+
+        self.builder = Gtk.Builder.new_from_string(TEST_XML, -1)
+        self.main_window = self.builder.get_object("window1")
+        self.main_window.show()
+
+        self.fixed = self.builder.get_object("fixed1")
+        self.button1 = self.builder.get_object("button1")
+        self.button1.set_label("XD")
+        self.pixbuf = GdkPixbuf.Pixbuf.new_from_file("../graphics/player.bmp")
+        image1 = Gtk.Image()
+        image1.set_from_pixbuf(self.pixbuf)
+        self.fixed.put(image1, 10, 10)
+        self.main_window.show_all()
+        #self.main_window.add(self.image)
+        #self.set_app_menu(builder.get_object("app-menu"))
+
+    def do_activate(self):
+        # We only allow a single window and raise any existing ones
+        if not self.window:
+            # Windows are associated with the application
+            # when the last one is closed the application shuts down
+            self.window = AppWindow(application=self, title="Main Window")
+
+        #self.window.present()
+
+    def do_command_line(self, command_line):
+        options = command_line.get_options_dict()
+        # convert GVariantDict -> GVariant -> dict
+        options = options.end().unpack()
+
+        if "test" in options:
+            # This is printed on the main instance
+            print("Test argument recieved: %s" % options["test"])
+
+        self.activate()
+        return 0
+
+    def on_about(self, action, param):
+        about_dialog = Gtk.AboutDialog(transient_for=self.window, modal=True)
+        about_dialog.present()
+
+    def on_quit(self, action, param):
+        self.quit()
+
+if __name__ == "__main__":
+    app = Application()
+    app.run(sys.argv)
+
